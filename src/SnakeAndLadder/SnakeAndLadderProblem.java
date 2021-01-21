ec823bb2bcd8475c1da115ea449236f0f02414f7
stepin105068 <69070085+stepin105068@users.noreply.github.com>
2021-01-21 12:44:14 +0530

    ADDED:Play of Snake, Ladder, PlayNo.

--- src/SnakeAndLadder/SnakeAndLadderProblem.java
+++ src/SnakeAndLadder/SnakeAndLadderProblem.java
@@ -1,14 +1,31 @@
 package SnakeAndLadder;
 
 public class SnakeAndLadderProblem {
-
+	public static final int IS_LADDER = 1;
+    public static final int IS_SNAKE = 2;
 	public static void main(String[] args) {
 		System.out.println("-----------Welcome to Snake And Ladder Problem---------");
 		int startingPos=0;
 	    int currentPos=0;
+	    int play;
+	    int dice;
+	    
 	    System.out.println("Player is Starting at: " + startingPos);
-	    int dice = (int) ((Math.floor(Math.random() * 10) % 6) + 1);
+	    dice = (int) ((Math.floor(Math.random() * 10) % 6) + 1);
         System.out.println("Current Dice Face is: " + dice);
+        play = (int) ((Math.floor(Math.random() * 10) % 3));
+        switch (play) {
+        	case IS_LADDER:
+        		currentPos += dice;
+        		break;
+        	case IS_SNAKE:
+        		currentPos -= dice;
+        		break;
+        	default:
+        		currentPos+=0;
+        		break;
+        }
+        System.out.println("New Position is: " + currentPos);
 	}
 
 }
