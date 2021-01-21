b5432ef0100f11f9483d24f88eec048e1708ac88
stepin105068 <69070085+stepin105068@users.noreply.github.com>
2021-01-21 12:18:21 +0530

    ADDED:WelcomeMessage

---
+++ src/SnakeAndLadder/SnakeAndLadderProblem.java
@@ -0,0 +1,10 @@
+package SnakeAndLadder;
+
+public class SnakeAndLadderProblem {
+
+	public static void main(String[] args) {
+		System.out.println("-----------Welcome to Snake And Ladder Problem---------");
+
+	}
+
+}
