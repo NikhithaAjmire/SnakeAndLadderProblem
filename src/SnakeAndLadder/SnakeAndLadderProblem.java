999bfcd029ed756b560d51972315aced320d6537
stepin105068 <69070085+stepin105068@users.noreply.github.com>
2021-01-21 12:22:25 +0530

    ADDED:Starting Position

--- src/SnakeAndLadder/SnakeAndLadderProblem.java
+++ src/SnakeAndLadder/SnakeAndLadderProblem.java
@@ -4,6 +4,9 @@ public class SnakeAndLadderProblem {
 
 	public static void main(String[] args) {
 		System.out.println("-----------Welcome to Snake And Ladder Problem---------");
+		int startingPos=0;
+	    int currentPos=0;
+	    System.out.println("Player is Starting at: " + startingPos);
 
 	}
 
